cb430c56442c7a29558d4d5286116f5d579ec3b5
gocando <gocandotiniacos@ufl.edu>
2025-09-21 16:37:35 -0400

    hello

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
-  // base can be omitted for root-domain deploys
-  plugins: [react()],
+  base: '/',              // <-- important
+  plugins: [react(), tailwind()],
 })
