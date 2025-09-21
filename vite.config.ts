6230c3860c1153cc07ed491a86d05f978047ec83
gocando <gocandotiniacos@ufl.edu>
2025-09-21 16:23:13 -0400

    hi3

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',               // correct for a custom domain
-  plugins: [react(), tailwind()],
-  build: {
-    assetsDir: 'assets',   // make sure CSS/JS go to /assets
-    sourcemap: false
-  }
+  // base can be omitted for root-domain deploys
+  base: '/portfolio/',
+  plugins: [react()],
 })
-
